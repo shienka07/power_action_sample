b8bdf32eaf42daf4000fe527ca43962ccc8b6211
Kyeonghye JO <shienka07.ko@gmail.com>
2025-02-10 11:11:20 +0900

    stop issue

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ async function makeIssue() {
     const token = process.env.GH_TOKEN; 
     const OWNER = "shienka07"; 
     const REPO = "power_action_sample";
-    const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-            title: "오늘 행운의 숫자",
-            body: `${Math.floor(Math.random() * 100) + 1}`,
-        })
-    });
+    // const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
+    //     method: 'POST',
+    //     headers: {
+    //         Authorization: `Bearer ${token}`,
+    //     },
+    //     body: JSON.stringify({
+    //         title: "오늘 행운의 숫자",
+    //         body: `${Math.floor(Math.random() * 100) + 1}`,
+    //     })
+    // });
 
     if(response.ok) {
         console.log("성공");
